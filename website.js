e56ade30161114bf18ab70c2cf5a557c161272e9
Patel enterprises <116709112+bhanuprakashchII67@users.noreply.github.com>
2026-09-21 00:32:42 +0530

    Bump website client version after header fix

--- website.js
+++ website.js
@@ -6,7 +6,7 @@ const siteDb=window.supabase?.createClient(SUPABASE_URL,SUPABASE_PUBLISHABLE_KEY
 const escSite=v=>String(v??"").replace(/[&<>"]/g,c=>({"&":"&amp;","<":"&lt;",">":"&gt;",'"':"&quot;"}[c]));
 const waPhone="919182725773";
 const phoneRE=/^[6-9]\d{9}$/;
-const SITE_VERSION="3.81.0";
+const SITE_VERSION="3.82.0";
 let siteErrorBusy=false;
 // Report every client-side website failure to CleanCore Manager's Error Finder.
 // This includes broken images, script failures, unhandled promise rejections,
